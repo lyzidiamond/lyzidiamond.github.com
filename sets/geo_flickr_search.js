f0af57408331cc47f2f441fcc71ccc0a2579f79d
Lyzi Diamond <lyzidiamond@gmail.com>
2014-03-31 15:14:11 -0700

    changes

--- sets/geo_flickr_search.js
+++ sets/geo_flickr_search.js
@@ -5,16 +5,12 @@ var flickr = new Flickr({
 var lat;
 var lon;
 
-//lat = 37.7839307645835;
-//lon = -122.483936163637;
-
 $('#input').submit(function(e) {
 
 	$('#result').innerHTML = '';
 
 	lat = parseFloat($('#lat').val());
 	lon = parseFloat($('#lon').val());
-	console.log(typeof lat);
 
 	flickr.photos.search({
 		lat: lat,
@@ -32,18 +28,3 @@ $('#input').submit(function(e) {
 	});
 	e.preventDefault();
 });
-
-/*flickr.photos.search({
-	lat: lat,
-	lon: lon,
-	radius: 0.3,
-	radius_units: 'mi',
-	text: 'graffiti'
-}, function(err, result) {
-	//if(err) { throw new Error(err); }
-	var returnedList = result.photos.photo;
-	for (var i = 0; i < returnedList.length; i++) {
-		var photoUrl = 'http://farm' + returnedList[i].farm + '.staticflickr.com/' + returnedList[i].server + '/' + returnedList[i].id + '_' + returnedList[i].secret + '.jpg';
-		document.getElementById('result').innerHTML += '<img src="' + photoUrl + '"><br><h2>' + returnedList[i].title + '</h2><br>';
-	};
-});*/
